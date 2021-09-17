722407a95dcbf53b6de534dc03a4bc503c054e21
Devlogs <tiendvlpA@gmail.com>
2021-09-17 14:20:53 +0700

    edit masa config

--- masa_backend/src/main/java/com/devlogs/masa_backend/common/Masa.java
+++ masa_backend/src/main/java/com/devlogs/masa_backend/common/Masa.java
@@ -12,11 +12,11 @@ public class Masa {
         public static String DATABASE_USER = "";
         public static String DATABASE_PASSWORD = "";
 
-    public static String GOOGLE_CLIENT_SECRET = "E0JTFqhxd-8vqtrsQPdKwAC0";
+    public static String GOOGLE_CLIENT_SECRET = "";
         public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/masa/logingoogle";
         public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
         public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
-        public static String CLIENT_ID = "608219007311-1f1l4barukaffu78oh3gqsadm865ci6p.apps.googleusercontent.com";
+        public static String CLIENT_ID = "";
         public static String GOOGLE_GRANT_TYPE = "authorization_code";
 
 
