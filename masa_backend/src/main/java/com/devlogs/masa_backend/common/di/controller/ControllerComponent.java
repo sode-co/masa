88b7ad3be9be1cf8c6cb20f607732905f1428816
Devlogs <tiendvlpA@gmail.com>
2021-09-17 18:13:18 +0700

    fix stub

--- masa_backend/src/main/java/com/devlogs/masa_backend/common/di/controller/ControllerComponent.java
+++ masa_backend/src/main/java/com/devlogs/masa_backend/common/di/controller/ControllerComponent.java
@@ -1,11 +1,9 @@
 package com.devlogs.masa_backend.common.di.controller;
 
 import com.devlogs.masa_backend.servlets.login.GoogleLoginProcessServlet;
-import com.devlogs.masa_backend.servlets.user.GetUserByEmailServlet;
 import dagger.Subcomponent;
 
 @Subcomponent (modules = {DataModule.class, RepositoryModule.class, ApiModule.class})
 public interface ControllerComponent {
-    void inject (GetUserByEmailServlet servlet);
     void inject(GoogleLoginProcessServlet googleLoginProcessServlet);
 }
