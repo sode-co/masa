e8daabfd3a82d30cfaa1569d01d802cbcf878a59
tiendvlp <tiendvlp@gmail.com>
2021-10-24 23:21:14 +0700

    update get meeting by id

--- masa_backend/src/main/java/com/devlogs/masa_backend/servlets/meeting/getmeeting/GetMeetingByIdServlet.java
+++ masa_backend/src/main/java/com/devlogs/masa_backend/servlets/meeting/getmeeting/GetMeetingByIdServlet.java
@@ -1,9 +1,13 @@
 package com.devlogs.masa_backend.servlets.meeting.getmeeting;
 
 import com.devlogs.masa_backend.common.annotations.AccessRole;
+import com.devlogs.masa_backend.domain.entities.MeetingEntity;
+import com.devlogs.masa_backend.domain.entities.UserEntity;
 import com.devlogs.masa_backend.meeting.GetMeetingByHostUseCase;
 import com.devlogs.masa_backend.meeting.GetMeetingByIdUseCase;
 import com.devlogs.masa_backend.servlets.common.base.BaseHttpServlet;
+import com.devlogs.masa_backend.servlets.meeting.common.GetMeetingResponse;
+import com.devlogs.masa_backend.user.GetUserByIdUseCase;
 import com.google.gson.Gson;
 
 import javax.inject.Inject;
@@ -12,6 +16,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Optional;
 
 import static com.devlogs.masa_backend.domain.entities.UserRole.TYPE.*;
 
@@ -21,7 +28,8 @@ public class GetMeetingByIdServlet extends BaseHttpServlet {
 
     @Inject
     protected GetMeetingByIdUseCase getMeetingByIdUseCase;
-
+    @Inject
+    protected GetUserByIdUseCase getUserByIdUseCase;
     @Override
     public void init() throws ServletException {
         super.init();
@@ -40,9 +48,22 @@ public class GetMeetingByIdServlet extends BaseHttpServlet {
         GetMeetingByIdUseCase.Result result = getMeetingByIdUseCase.executes(meetingId);
         if (result instanceof GetMeetingByIdUseCase.Result.ConnectionError) {
             getRequestComponent().getResponseHelper().responseMessage(500, "Connection to db error");
-        } else if ( result instanceof GetMeetingByIdUseCase.Result.Success) {
-            String json = new Gson().toJson(result);
-            getRequestComponent().getResponseHelper().responseJsonOk(json);
+        }
+        if (result instanceof GetMeetingByIdUseCase.Result.NotFound) {
+            getRequestComponent().getResponseHelper().responseMessage(404, "Meeting not found");
+        }
+        else if ( result instanceof GetMeetingByIdUseCase.Result.Success) {
+            GetUserByIdUseCase.Result getUserResult = getUserByIdUseCase.executes(((GetMeetingByIdUseCase.Result.Success) result).meeting.getHostId());
+            if (getUserResult instanceof GetUserByIdUseCase.Result.Success) {
+                MeetingEntity meeting = ((GetMeetingByIdUseCase.Result.Success) result).meeting;
+                UserEntity host = ((GetUserByIdUseCase.Result.Success) getUserResult).getUser();
+                GetMeetingResponse.Data responseData = new GetMeetingResponse.Data(meeting.getId(),meeting.getTitle(),meeting.getPlatform(), meeting.getTopic(), host, meeting.getStartTime(), meeting.getEndTime(), meeting.getDescription());
+                String json = new Gson().toJson(responseData);
+                getRequestComponent().getResponseHelper().responseJsonOk(json);
+            } else {
+                getRequestComponent().getResponseHelper().responseMessage(404, "Host not found");
+            }
+
         }
     }
 }
